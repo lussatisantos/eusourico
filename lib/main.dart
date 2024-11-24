aafa2002fd67bd00835e5cac0b0bcafa2d14a0c8
Lussati Santos <lussatisantos10@gmail.com>
2024-11-24 12:30:28 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Ola Mundo!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(child: Text('Eu Sou Rico'), ),
+        ),
       ),
     ),
   );
